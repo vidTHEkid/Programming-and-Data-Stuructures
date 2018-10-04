f524d7846c57aa7039f1c3bad3475caa3aff177f
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-10-04 11:20:11 +0100

    update

---
+++ WS01BFT/furtherTasks/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Further task
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your age? (in Years)");
+            int ageY = Convert.ToInt32(Console.ReadLine());
+
+            int age = ageY * 365;
+
+            Console.WriteLine("Hello {0}. You are {1} days old.", name, age);
+
+
+            Console.ReadLine();
+        }
+    }
+}
